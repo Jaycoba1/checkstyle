1a0c5756b68056a2d4a6355e93cd594525a37428
Jonah <jonah.ting@hotmail.com>
2026-05-25 17:10:37 +1000

    init bad try check

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/BadTryCheck.java
@@ -0,0 +1,8 @@
+package com.puppycrawl.tools.checkstyle.checks;
+
+/**
+ * Class is designed to catch bad utilisation of try blocks. Specifically catches if excess
+ * code is inside a try block
+ */
+public class BadTryCheck {
+}
